447e33530d676864e2af8a21fd2f3b9aa9812e08
Andrey-Engin <churandos@gmail.com>
2024-01-08 19:03:53 +0300

    Раотает со следующими багами ♫ неправильная нумерация светодиодов ♫ буффер передаваемых данных смещён ♫ 	работает следующим образом 		ARGB_SetRGB(0, 60, 60, 60); // 1 diod R and G 	    //ARGB_SetRGB(1, 60, 60, 60); // null 	    ARGB_SetRGB(2, 0, 0, 60); // 1 diod B and 2 diod R and G 	    //ARGB_SetRGB(3, 60, 60, 60); // null

--- Core/app_main.c
+++ Core/app_main.c
@@ -11,14 +11,47 @@ void app_main(void)
 {
 	 ARGB_Init();  // Initialization
 
-	    ARGB_Clear(); // Clear stirp
-	    while (ARGB_Show() != ARGB_OK); // Update - Option 1
+	 ARGB_Clear(); // Clear stirp
+	 while (ARGB_Show() != ARGB_OK); // Update - Option 1
 
-	    ARGB_SetBrightness(100);  // Set global brightness to 40%
+	 int i = 0;
+	 while(i<1000)
+	 {
+	    ARGB_SetBrightness(40);  // Set global brightness to 40%
 
-	    ARGB_SetHSV(0, 0, 255, 255); // Set LED №1 with Red
+
+	    //ARGB_FillRGB(60,60,60);
+	    ARGB_SetRGB(0, 60, 60, 60); // Set LED №3 with 255 Green
+	    //ARGB_SetRGB(1, 60, 60, 60); // Set LED №3 with 255 Green
+	    ARGB_SetRGB(2, 0, 0, 60); // Set LED №3 with 255 Green
+	    //ARGB_SetRGB(3, 60, 60, 60); // Set LED №3 with 255 Green
+	    ARGB_SetRGB(4, 60, 60, 60); // Set LED №3 with 255 Green
+	    //ARGB_SetRGB(5, 60, 60, 60); // Set LED №3 with 255 Green
+	    ARGB_SetRGB(6, 60, 60, 60); // Set LED №3 with 255 Green
+	    while (!ARGB_Show());  // Update - Option 2
+
+	    HAL_Delay(500);
+	    i=i+1;
+
+	 }
+	    //ARGB_FillRGB(200, 0, 0); // Fill all the strip with Red
+	    //    while (!ARGB_Show());
+
+
+	    /*
+	    int numb_diode = 0;
+	    while(1)
+	    {
+
+	    ARGB_SetHSV(numb_diode, 0, 255, 0); // Set LED №1 with green
 	    while (!ARGB_Ready()); // Update - Option 3
 	    ARGB_Show();
+
+	    HAL_Delay(1000);
+	    numb_diode++;
+	    if(numb_diode > 59)
+	    	numb_diode = 0;
+	    }*/
 
 	/*while(1)
 	{
